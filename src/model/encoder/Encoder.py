79b1e60c0a9a8697b81b4a7e184b9946c88cface
TonyXYJ <xiao_yingjie@qq.com>
2024-02-11 23:56:55 +0800

    Update the PostionalEncoding and others.

--- src/model/encoder/Encoder.py
+++ src/model/encoder/Encoder.py
@@ -7,11 +7,13 @@
  '''
 
 
+import math
 from typing import List
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.functions import *
+from MultiheadAttention import *
 
 
 class Embeddings(nn.Module):
@@ -24,6 +26,7 @@ class Embeddings(nn.Module):
         super(Embeddings, self).__init__()
         self.embedding = nn.Embedding(vocabSize, d_model)
         self.d_model = d_model
+        self.register_buffer('vocabEmbedding', self.embedding)
 
     def forward(
         self, 
@@ -32,6 +35,36 @@ class Embeddings(nn.Module):
         # HACK
         # encoder or decoder?
         return self.embedding(x) * (self.d_model**0.5)
+    
+
+class PostionalEncoding(nn.Module):
+    """
+    PE(pos, 2i) = sin(pos / 10000^{2i/d_model})
+    PE(pos, 2i+1) = cos(pos / 10000^{2i/d_model})
+    """
+    def __init__(
+        self, 
+        d_model: int, 
+        dropoutRate: float, 
+        maxSeqLen=5000
+    ) -> None:
+        super(PostionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropoutRate)
+        pe = torch.zeros(maxSeqLen, d_model)
+        position = torch.arange(0, maxSeqLen).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(
+        self, 
+        x: torch.Tensor
+    ):
+        return self.dropout(x + nn.Parameter(self.pe[:, : x.size(1)], requires_grad=True))
 
 
 class LayerNorm(nn.Module):
@@ -76,13 +109,33 @@ class SublayerConnection(nn.Module):
         return x + self.dropout(sublayer(self.norm(x)))
 
 
+class PositionwiseFeedforward(nn.Module):
+    """FFN(x) = max(0, x W_1 + b_1) W_2 + b_2"""
+    def __init__(
+        self, 
+        d_model: int, 
+        d_ff: int, 
+        dropoutRate=0.1, 
+    ) -> None:
+        super(PositionwiseFeedforward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropoutRate)
+
+    def forward(
+        self, 
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+
 class EncoderLayer(nn.Module):
     """Encoder layer == self-attn + feedforward"""
     def __init__(
         self, 
         size: int, 
-        selfAttn: nn.Module, 
-        feedforward: nn.Module, 
+        selfAttn: MultiheadAttention, 
+        feedforward: PositionwiseFeedforward, 
         dropoutRate: float
     ) -> None:
         super(EncoderLayer, self).__init__()
@@ -100,33 +153,11 @@ class EncoderLayer(nn.Module):
         return self.sublayers[1](x, self.feedforward)
 
 
-class PositionwiseFeedforward(nn.Module):
-    """FFN(x) = max(0, x W_1 + b_1) W_2 + b_2"""
-    def __init__(
-        self, 
-        d_model: int, 
-        d_ff: int, 
-        dropoutRate=0.1
-    ) -> None:
-        super(PositionwiseFeedforward, self).__init__()
-        self.w_1 = nn.Linear(d_model, d_ff)
-        # HACK
-        # w_2 is typically shared with the embedding layer.
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropoutRate)
-
-    def forward(
-        self, 
-        x: torch.Tensor
-    ) -> torch.Tensor:
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
-        
-
 class Encoder(nn.Module):
     """A stack of L encoder layers."""
     def __init__(
         self, 
-        encoderLayer: nn.Module, 
+        encoderLayer: EncoderLayer, 
         layerNum: int
     ) -> None:
         super(Encoder, self).__init__()
